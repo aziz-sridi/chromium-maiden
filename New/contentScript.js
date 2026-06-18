469793982da64975224a68547c397c4b73fcf09d
aziz <medazizsridi@gmail.com>
2026-06-18 18:40:00 +0100

    chore: remove generated and duplicate artifacts

--- New/contentScript.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  if (!window.ExtractorSelection) {
-    console.error("ExtractorSelection module not loaded");
-    return;
-  }
-
-  window.ExtractorSelection.setupKeyboardFallback();
-  window.ExtractorSelection.setupRuntimeListener();
-})();
